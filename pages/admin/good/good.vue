e43228cab788994c0856eaba9feee0d9a4c359d2
wyk861417548 <861417548@qq.com>
2021-08-26 13:47:52 +0800

    新增商品类别接口对接完成

--- pages/admin/good/good.vue
+++ pages/admin/good/good.vue
@@ -2,8 +2,8 @@
 	<view class="j-full-curbox page-bg">
 		<view class="box">
 			<view class="box-title">商品类别</view>
-			<input type="text" value="" readonly placeholder="请选择" @click="show = true" />
-			<u-select v-model="show" :list="list" @confirm="confirm"></u-select>
+			<input type="text" v-model="selCategory.name" readonly placeholder="请选择" @click="show = true" />
+			<u-select v-model="show" :list="goods" @confirm="confirm"></u-select>
 		</view>
 		
 		<view class="box">
@@ -11,30 +11,26 @@
 			
 			<view class="column">
 				<view class="left">名称</view>
-				<input type="text" value="" placeholder="请输入商品名称"/>
+				<input type="text" v-model="data.name" placeholder="请输入商品名称"/>
 			</view>
 			
 			<view class="column">
 				<view class="left">价格</view>
-				<input type="text" value="" placeholder="请输入商品价格"/>
+				<input type="text" v-model="data.price" placeholder="请输入商品价格"/>
 			</view>
 			
-			<view class="column">
-				<view class="left"></view>
-				<input type="text" value="" placeholder="请输入商品名称"/>
-			</view>
 			
 			<view class="column">
 				<view class="left">简介</view>
-				<input type="text" value="" placeholder="请输入商品简介"/>
+				<input type="text" v-model="data.introduct"  placeholder="请输入商品简介"/>
 			</view>
-			
 			
 			
 			<view class="column">
-				<view class="left"></view>
-				<input type="text" value="" placeholder="请输入商品名称"/>
+				<view class="left">规格</view>
+				<input type="text" v-model="data.mode" readonly/>
 			</view>
+			
 			
 			
 		</view>
@@ -59,9 +55,16 @@
 		data() {
 			return {
 				show:false,
+				
 				// 商品类别
-				list: [
-				],
+				goods: [],
+				
+				// 当前所选商品类别信息
+				selCategory:{
+					_id:"",
+					
+					name:"",
+				},
 				
 				data:{
 					// 商品类别
@@ -76,6 +79,9 @@
 					// 数量
 					num:"",
 					
+					// 规格
+					mode:"默认",
+					
 					// 简介
 					introduct:"",
 					
@@ -89,20 +95,34 @@
 		},
 		methods: {
 			// 获取商品类别
+			// 获取商品类别
 			getCategory(){
 				let data = {
 					action:"getCategory"
 				}
 				
 				this.$cloud.cloudFn(data).then(res=>{
-					console.log("getCategory",res);
-					this.list = res.data;
+					if(res.data && res.data.length > 0){
+						res.data.map(item=>{
+							console.log("item",item);
+							this.goods.push({
+								value: item._id,
+								label: item.name
+							})
+						})
+						
+					}
+					
 				});
 			},
 			
 			// 选中商品类别
 			confirm(e) {
-				console.log(e);
+				console.log("e",e);
+				this.selCategory = {
+					name:e[0].label,
+					_id:e[0].value
+				} 
 			}
 		}
 	}
--- pages/admin/good/index.vue
+++ pages/admin/good/index.vue
@@ -5,25 +5,71 @@
 		<view class="flex container" style="flex:1;background-color: #108EE9;">
 			<!-- :scroll-top="verticalNavTop" -->
 			<scroll-view class="left" :scroll-with-animation='true'>
+				<view  @click="showTip"> 新增商品类别</view>
+				
 				<view class="menu" :id="`menu-${item.id}`" v-for="(item, index) in goods" :key="index" :class="{'current': item.id == currentIndex}"  @tap="handleMenuTap(item.id)">
 					<text>{{ item.name }}</text>
 				</view>
 				
-				<view  @click="showTip"> 新增商品类别</button>
+				
 			</scroll-view>
 			
 			<scroll-view class="right"   :scroll-with-animation='true'>
-				<view  @click="$skip" data-url="/pages/admin/good/good"> 新增商品</button>
+				<view  @click="$skip" data-url="/pages/admin/good/good"> 新增商品</view>
 				
-				<view class="menu" :id="`menu-${item.id}`" v-for="(item, index) in goods" :key="index" :class="{'current': item.id == currentIndex}"  @tap="handleMenuTap(item.id)">
-					<text>{{ item.name }}</text>
+				<view class="category" v-for="(category, index) in goods" :key="index" :id="`cate-${category.id}`">
+					<!-- 商品类别 -->
+					<view class="title">
+						<text v-if="index == 0"></text>
+						<text v-if="index != 0">{{category.name }}{{category.child.length}}</text>
+					</view>
+					{{category.child}}
+					<!-- 商品循环 -->
+					<view class="good" v-for="(product, key) in category.child" :key="key">
+						<view class="" style="height: 20rpx;"></view>
+						<view class="flex">
+							<view class="image" :style="{background:'url('+product.image+') no-repeat center / cover;'}" @tap='$skip' :data-url="'/pages/store/stock/goodDetail?id='+product.id"></view>
+											
+							<view class="right">
+								<!-- 商品名称 -->
+								<view>{{ product.storeName }}</view>
+								<!-- 商品描述 -->
+								<text class="tips text-overflowMut">{{ product.content }}</text>
+								
+								<view class="price_and_action j-flex" style="width: 100%;">
+									<text class="price colfc4">￥{{ product.price }}</text>
+									
+				
+								<!-- 	<Action v-if="product.productAttr.length < 2" :num="goodCartNum(product.id)" @reduce="handleReduceToCart(product)" @add="handleAddToCart(product,1)"></Action>
+							
+									<text class="norm" v-if="product.productAttr.length > 1" @tap="showGoodDetailModal(product)">选规格</text> -->
+								</view>
+								
+							</view>
+						</view>
+					</view>					
 				</view>
 			</scroll-view>
 			
 		</view>
 		
 
-		<!-- <um-alert></um-alert> -->
+		<um-alert ref="category">
+			<template>
+				<view class="box">
+					<view class="box-title">新增商品类别</view>
+					<view class="column">
+						<text class="left" style="width: 200rpx;">类别名称</text>
+						<input class="right" style="width: 300rpx;" type="text" v-model="category.name" placeholder="请输入商品类别名称"/>
+					</view>
+					
+					<view class="mt-20" style="text-align: right;">
+						<text class="mr-10" @click="close">取消</text>
+						<text class="col167" @click="confirm">确定</text>
+					</view>
+				</view>
+			</template>
+		</um-alert>
 		
 		<!-- 自定义tabBar组件 -->
 		<view class="" style="height: 100rpx;">
@@ -40,30 +86,65 @@
 			return {
 				show: false,
 				
+				// 商品信息
+				goods:[],
+				
 				// 商品类别信息
 				category:{
+					// 商品类别名称
 					name:"",
 					
+					// 用处存放商品容器
 					child:[]
 				}
 			}
 		},
+		
+		onLoad() {
+			this.getCategory();
+		},
+		
 		methods: {
-			// 新增商品类别
-			addCategory(){
+			// 获取商品类别
+			getCategory(){
 				let data = {
-					action:"addCategory"
+					action:"getCategory"
 				}
 				
 				this.$cloud.cloudFn(data).then(res=>{
-					console.log("addCategory",res);
-					this.list = res.data;
+					console.log("getCategory",res);
+					this.goods = res.data;
+				});
+			},
+			
+			// 新增商品类别
+			addCategory(){
+				let data = {
+					action:"addCategory",
+					params:this.category
+				}
+				
+				this.$cloud.cloudFn(data).then(res=>{
+					uni.showToast({title:"添加成功",icon:"none"})
+					this.getCategory();
 				});
 			},
 			
 			// 新增商品类别弹窗
 			showTip(){
-				this.$ref.category = true;
+				console.log("this",this.$refs);
+				this.$refs.category.show = true;
+			},
+			
+			// 确定弹窗事件
+			confirm(){
+				this.addCategory();
+				this.$refs.category.show = false;
+			},
+			
+			// 关闭弹窗事件
+			close(){
+				this.$refs.category.show = false;
 			}
 		}
 	}
@@ -78,6 +159,8 @@
 	.container{
 		.left{
 			width: 30%;
+			background-color: #fff;
+			border-right: 2rpx solid rgba(221,221,221,.3);
 		}
 	}
 </style>
